10ceeedf2a2809ce5942c39503d1d98a46166db1
Saul Corona <saulcoronasantos@ymail.com>
2021-04-03 15:03:07 -0600

    Functions and Interfaces

--- ejercicios/typescript/index.ts
+++ ejercicios/typescript/index.ts
@@ -38,3 +38,61 @@ comodin = {type: 'Wildcard'};
 //Object
 let someObject:object = {type: 'wildcard'}
 // console.log(someObject)
+
+//Funciones
+
+function add(a:number, b:number):number{
+     return a+b;
+}
+
+const sum = add(5, 6);
+
+function createAdder(a:number): (number) => number{
+     return function(b:number){
+          return b + a;
+     }
+}
+
+const addFour = createAdder(4);
+const fourPlus6 = addFour(6);
+
+// console.log(fourPlus6)
+
+function fullname(firsName: string, lastname?:string): string{
+     return `${firsName} ${lastname}`
+}
+
+const saul = fullname("Saul", "Corona");
+
+// console.log(saul)
+
+enum Color {
+    Negro = "Negro",
+    Morado = "Morado",
+     Rosa = "Rosa",
+}
+
+interface Rectangulo {
+     ancho: number,
+     alto: number,
+     color: Color,
+}
+
+let rect:Rectangulo = {
+     ancho: 4,
+     alto: 6,
+     color:Color.Rojo,
+}
+
+function area(r:Rectangulo){
+     return r.alto * r.ancho
+}
+
+const areaReact = area(rect);
+console.log(areaReact);
+
+ rect.toString = function(){
+     return this.color ? `Un rectangulo ${this.color}` : `Un rectangulo`;
+}
+
+console.log(rect.toString())
