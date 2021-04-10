7410cdc8067e372ecf9e2609cb9969c86d52fc1c
Saul Corona <saulcoronasantos@ymail.com>
2021-04-09 19:54:49 -0500

    Add Ads plugin

--- assets/MediaPlayer.ts
+++ assets/MediaPlayer.ts
@@ -1,36 +1,36 @@
 class MediaPlayer {
   media: HTMLMediaElement;
   plugins: Array<any>;
+  container: HTMLElement;
 
   constructor(config) {
     this.media = config.el;
     this.plugins = config.plugins || [];
-
+    this.initPlayer();
     this.initPlugins();
   }
-  private initPlugins() {
-    const player = {
-      play: () => this.play(),
-      pause: () => this.pause(),
-      media: this.media,
-      get muted() {
-        return this.media.muted;
-      },
-      set muted(value) {
-        this.media.muted = value;
-      }
-    };
 
+  initPlayer() {
+    this.container = document.createElement('div');
+    this.container.style.position = 'relative';
+    this.media.parentNode.insertBefore(this.container, this.media);
+    this.container.appendChild(this.media);
+  }
+
+  private initPlugins() {
     this.plugins.forEach(plugin => {
       plugin.run(this);
     });
   }
+
   play() {
     this.media.play();
   }
+
   pause() {
     this.media.pause();
   }
+
   togglePlay() {
     if (this.media.paused) {
       this.play();
@@ -38,18 +38,14 @@ class MediaPlayer {
       this.pause();
     }
   }
+
   mute() {
     this.media.muted = true;
   }
+
   unmute() {
     this.media.muted = false;
   }
 }
-
-
-
-
-
-
 
 export default MediaPlayer;
--- assets/index.ts
+++ assets/index.ts
@@ -1,6 +1,7 @@
 import MediaPlayer from './MediaPlayer';
 import AutoPlay from './plugins/AutoPlay';
 import AutoPause from './plugins/AutoPause';
+import Ads from './plugins/Ads'
 
 const video = document.querySelector('video');
 const player = new MediaPlayer({ 
@@ -8,6 +9,7 @@ const player = new MediaPlayer({
     plugins:[
       new AutoPlay(),
       new AutoPause(),
+      new Ads(),
     ] 
   });
 
